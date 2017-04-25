7d2065be59c3594a12ae2b12b53fabac6a3dd10a
Anshdeep Singh <anshdeep.1494@gmail.com>
2017-04-25 13:09:07 +0530

    Step 1: Create a Java Library - Completed

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeGenerator;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        // Getting the joke from our Java library libjokes
+        JokeGenerator jokeGenerator = new JokeGenerator();
+        String joke = jokeGenerator.getRandomJoke();
+
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
     }
 
 
